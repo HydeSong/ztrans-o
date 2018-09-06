05b38d18e42ee05d7123dedc3c1a5fcb5a5f7555
Hyde Song <hyde.song@yahoo.com>
2018-09-06 22:36:14 +0800

    'recove'

--- src/components/User/AddressInfo.vue
+++ src/components/User/AddressInfo.vue
@@ -13,18 +13,18 @@
                         :value="shippingDistrictDetail">
                 </md-field-item>
                 <md-input-item
-                        v-model="shipping.addressDetail"
+                        v-model="addressDetail"
                         title="详细地址"
                         placeholder="请输入详细地址(必填)"
                 ></md-input-item>
                 <md-input-item
                         type="phone"
-                        v-model="shipping.personMobile"
+                        v-model="personMobile"
                         title="联系电话"
                         placeholder="请填写联系电话(必填)"
                 ></md-input-item>
                 <md-input-item
-                        v-model="shipping.personName"
+                        v-model="personName"
                         title="发货人姓名"
                         placeholder="请填写发货人姓名(可选填)"
                 ></md-input-item>
@@ -33,13 +33,13 @@
                         title="发货时间"
                         arrow="arrow-right"
                         align="right"
-                        :value="shipping.goodsTime"
+                        :value="goodsTime"
                         @click.native="isDatePickerShow = true">
                 </md-field-item>
                 <md-input-item
                         title="发货点个数"
                         type="digit"
-                        v-model="shipping.locationNum"
+                        v-model="locationNum"
                         placeholder="请输入发货点个数"
                 ></md-input-item>
             </md-field>
@@ -53,25 +53,25 @@
                         :value="receiveDistrictDetail">
                 </md-field-item>
                 <md-input-item
-                        v-model="receiver.addressDetail"
+                        v-model="raddressDetail"
                         title="详细地址"
                         placeholder="请输入详细地址(必填)"
                 ></md-input-item>
                 <md-input-item
                         type="phone"
-                        v-model="receiver.personMobile"
+                        v-model="rpersonMobile"
                         title="联系电话"
                         placeholder="请填写联系电话(必填)"
                 ></md-input-item>
                 <md-input-item
-                        v-model="receiver.personName"
+                        v-model="rpersonName"
                         title="收货人姓名"
                         placeholder="请填写收货人姓名(可选填)"
                 ></md-input-item>
                 <md-input-item
                         title="收货点个数"
                         type="digit"
-                        v-model="receiver.locationNum"
+                        v-model="rlocationNum"
                         placeholder="请输入收货点个数"
                 ></md-input-item>
             </md-field>
@@ -113,6 +113,15 @@
     },
     data () {
       return {
+        raddressDetail: '',
+        rpersonMobile: '',
+        rpersonName: '',
+        rlocationNum: '',
+        personMobile: '',
+        personName: '',
+        addressDetail: '',
+        goodsTime: '',
+        locationNum: '',
         currentDate: new Date(),
         isDatePickerShow: false,
       }
@@ -139,10 +148,23 @@
       confirm () {
         if (this.isShipping) {
           console.log('确认发货信息')
-          this.setShipping(this.shipping)
+          let addressInfo = {
+            personMobile: this.personMobile,
+            personName: this.personName,
+            addressDetail: this.addressDetail,
+            goodsTime: this.goodsTime,
+            locationNum: this.locationNum
+          }
+          this.setShipping(addressInfo)
         } else {
           console.log('确认收货信息')
-          this.setReceiver(this.receiver)
+          let addressInfo = {
+            personMobile: this.rpersonMobile,
+            personName: this.rpersonName,
+            addressDetail: this.raddressDetail,
+            locationNum: this.rlocationNum
+          }
+          this.setReceiver(addressInfo)
         }
         this.$router.go(-1)
       },
